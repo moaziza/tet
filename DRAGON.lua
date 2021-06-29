18686afdb79ff162b506109b344f11f18b280f2e
moaziza <85038472+moaziza@users.noreply.github.com>
2021-06-30 00:13:15 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -12729,8 +12729,8 @@ keyboard = {}
 keyboard.inline_keyboard = {
 {{text = '𝙳𝙴𝚅 𝙰𝙻𝙰𝚉𝙸𝚉𝚈 ◉',url="t.me/alazizy"}},
 }
-https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo=https://t.me/ ..us.. &caption=  .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-return false
+local msg_id = msg.id_/2097152/0.5
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/mmsst13/870 &caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
 if text == "اسمي"  then 
